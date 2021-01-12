86b691396c6f6c9c313c37150bd6df6a2e1d55a4
Bunlert Khongkaho <bunlert.khongkaho@gmail.com>
2021-01-12 09:30:39 +0700

    update readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
 import { Home, Login } from "./pages";
 
 export default function App() {
   return (
-    <Fragment>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-
+    <>
       <Switch>
         <Route path="/login" component={Login} />
         <Route exact path="/" component={Home} />
       </Switch>
-    </Fragment>
+    </>
   );
 }
